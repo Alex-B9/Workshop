07e4639d1d73420c40d7d3251f83a6ef8ef582fe
JasonFelgenhauer <jasonfel1@outlook.be>
2022-08-09 10:05:42 +0200

    t

--- front/src/views/Tasks.js
+++ front/src/views/Tasks.js
@@ -7,7 +7,7 @@ const Tasks = () => {
 
 	useEffect(() => {
 		if (playOnce) {
-			axios.get('http://45.140.164.217:3005/api/tasks').then((res) => {
+			axios.get('http://localhost:3001/api/tasks').then((res) => {
 				setData(res.data.data);
 				setPlayOnce(false);
 			});
@@ -17,7 +17,7 @@ const Tasks = () => {
 	const addTask = async (e) => {
 		e.preventDefault();
 
-		await axios.post('http://45.140.164.217:3005/api/add', { title: e.target.task.value }).then((res) => {
+		await axios.post('http://localhost:3001/api/add', { title: e.target.task.value }).then((res) => {
 			setPlayOnce(true);
 		});
 	};
@@ -25,7 +25,7 @@ const Tasks = () => {
 	const doneTask = async (e) => {
 		const id = e.target.id;
 
-		await axios.put(`http://45.140.164.217:3005/api/update/${id}`, { done: true }).then((res) => {
+		await axios.put(`http://localhost:3001/api/update/${id}`, { done: true }).then((res) => {
 			setPlayOnce(true);
 		});
 	};
@@ -33,7 +33,7 @@ const Tasks = () => {
 	const deleteTask = async (e) => {
 		const id = e.target.id;
 
-		await axios.delete(`http://45.140.164.217:3005/api/delete/${id}`).then((res) => {
+		await axios.delete(`http://localhost:3001/api/delete/${id}`).then((res) => {
 			setPlayOnce(true);
 		});
 	};
